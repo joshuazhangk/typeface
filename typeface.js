118aba7fcca0cf54238175968b759a4634e6c9a5
joshuazhangk <59916172+joshuazhangk@users.noreply.github.com>
2024-07-29 20:44:49 +0800

    Update typeface.js

--- typeface.js
+++ typeface.js
@@ -266,7 +266,7 @@ const scroll = () => {
         scrollTrigger: {
           trigger: title,
           start: "top bottom+=40%",
-            end: "top top+=90%",
+            end: "top top+=5%",
           scrub: true
         }
       }
